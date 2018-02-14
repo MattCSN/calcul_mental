a7e94d53a9fb7e5bf5db9cd57a51deac4b916547
Sebastien George <sebastien.george@hei.yncrea.fr>
2018-02-14 16:28:40 +0100

    Qcm class

--- calcul_mental_core/src/main/java/entity/Qcm.java
+++ calcul_mental_core/src/main/java/entity/Qcm.java
@@ -3,10 +3,15 @@ package entity;
 public class Qcm {
 
     private String question;
+
+
+
     private String propositionA;
     private String propositionB;
     private String propositionC;
     private  String propositionD;
+    private int difficulty;
+    private String category;
 
     public Qcm() {
     }
@@ -20,7 +25,15 @@ public class Qcm {
         this.propositionD = propositionD;
     }
 
-
+    public Qcm(String question, String propositionA, String propositionB, String propositionC, String propositionD, int difficulty, String category) {
+        this.question = question;
+        this.propositionA = propositionA;
+        this.propositionB = propositionB;
+        this.propositionC = propositionC;
+        this.propositionD = propositionD;
+        this.difficulty = difficulty;
+        this.category = category;
+    }
 
     public String getQuestion() {
         return question;
