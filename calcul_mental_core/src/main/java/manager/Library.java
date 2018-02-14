4d48a23dbeeb89352b8f6f968529ba4d86b2d26b
Sebastien George <sebastien.george@hei.yncrea.fr>
2018-02-14 16:00:21 +0100

    complete manager

--- calcul_mental_core/src/main/java/manager/Library.java
+++ calcul_mental_core/src/main/java/manager/Library.java
@@ -6,6 +6,8 @@ import dao.impl.CalculDAOImpl;
 public class Library {
 
 
+    private int firstnumber;
+
     private static class CalculLibraryHolder {
         private final static Library instance = new Library();
     }
@@ -19,6 +21,31 @@ public class Library {
 
     }
 
-
-
+    public void add(){
+        calculDAO.add();
+    }
+    public void multiply(){
+        calculDAO.multiply();
+    }
+    public void divide(){
+        calculDAO.divide();
+    }
+    public int getFirstNumber(){
+       return calculDAO.getFirstNumber();
+    }
+    public void setFirstNumber(int firstNumber){
+       calculDAO.setFirstNumber(firstNumber);
+    }
+    public int getResult(){
+        return calculDAO.getResult();
+    }
+    public void setResult(int result){
+        calculDAO.setResult(result);
+    }
+    public int getSecondNumber(){
+        return calculDAO.getSecondNumber();
+    }
+    public void setSecondNumber(int secondNumber){
+        calculDAO.setSecondNumber(secondNumber);
+    }
 }
