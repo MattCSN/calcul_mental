80a3fd2f0e20b1f4181abcb4a2fee4d1afd6f078
AntoineNormand <antoine.normand@hei.yncrea.fr>
2018-02-14 15:23:42 +0100

    [Calcul Entity] Création de calcul

---
+++ calcul_mental_core/src/main/java/entity/Calcul.java
@@ -0,0 +1,32 @@
+package main.java.entity;
+
+public class Calcul {
+
+    private int firstNumber;
+    private int secondNumber;
+    private int result;
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
