93d4f980ff7d965ae0339bedeaada45ca2de6654
Joseph Sallembien <josallembien@gmail.com>
2018-02-20 18:04:28 +0100

    change?

--- calcul_mental_web/src/main/java/calcul/web/controller/CalculController.java
+++ calcul_mental_web/src/main/java/calcul/web/controller/CalculController.java
@@ -1,19 +1,11 @@
 package calcul.web.controller;
 
-import calcul.core.entity.Calcul;
 import calcul.core.service.CalculService;
-import calcul.core.service.impl.CalculServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
-import java.util.List;
 
 @Controller
 public class CalculController {
