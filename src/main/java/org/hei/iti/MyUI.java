94d2b4de785a204e4c9d5ff5db7d7ce701d774fd
matt <matthieu.coisnon@hei.fr>
2018-02-23 17:37:48 +0100

    [INIT] page poke

---
+++ src/main/java/org/hei/iti/MyUI.java
@@ -0,0 +1,47 @@
+package org.hei.iti;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class MyUI extends UI {
+
+    int poke = 0;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+
+        final Label pokeText = new Label("Poke : " + poke);
+
+        Button button = new Button("Poke Me");
+        button.addClickListener(e -> {
+            poke ++;
+            pokeText.setValue("Poke : " + poke);
+        });
+        
+        layout.addComponents(pokeText, button);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
