94d806fb21ee061301c9af67366957dc3610ca15
Minetactics <josallembien@gmail.com>
2018-03-17 19:30:27 +0100

    structure et fichiers DAO, service, impl et config git statusProbleme avec le driver jdbc pour postgres

--- calcul_mental_web/src/main/java/web/config/WSConfig.java
+++ calcul_mental_web/src/main/java/web/config/WSConfig.java
@@ -1,4 +1,45 @@
 package web.config;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import web.controller.RestController;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@ComponentScan(basePackages = "web.controller")
+@ImportResource({"classpath:META-INF/cxf/cxf.xml"})
 public class WSConfig {
+
+    @Inject
+    private Bus cxfBus;
+
+    @Inject
+    private List<RestController> controllers;
+
+    @Bean
+    public Server jaxrsServer(JacksonJsonProvider jsonProvider){
+        JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
+        List<Object> serviceBeans = new ArrayList<>();
+        serviceBeans.addAll(controllers);
+        sf.setServiceBeans(serviceBeans);
+        sf.setProviders(Arrays.asList(jsonProvider));
+        sf.setAddress("/");
+        sf.setBus(cxfBus);
+        return sf.create();
+    }
+
+    @Bean
+    public JacksonJsonProvider jsonProvider() {
+        return new JacksonJsonProvider();
+    }
 }
