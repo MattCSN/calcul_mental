dffb7e82a5269c0e52074b5997ab851ced776fb0
annweytens <anne-sophie.weytens@hei.yncrea.fr>
2018-03-22 12:29:28 +0100

    Modifications pour MySQL

--- calcul_mental_core/src/main/java/core/config/MainConfig.java
+++ calcul_mental_core/src/main/java/core/config/MainConfig.java
@@ -16,29 +16,12 @@ import java.util.Properties;
         @Bean
         public Properties dbProperties() throws IOException, URISyntaxException {
 
-            /*
-            ENVIRONMENT VARIABLE DATABASE_URL SHOULD BE SET
-                                                            -- for Mac and Linux
-                                                                    export DATABASE_URL=postgres://$(whoami)
-                                                            -- for Windows
-                                                                    set DATABASE_URL=postgres://$(whoami)
-            URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
             Properties props = new Properties();
-            props.setProperty("jdbcUrl","jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require");
-            props.setProperty("username",dbUri.getUserInfo().split(":")[0]);
-            props.setProperty("password", dbUri.getUserInfo().split(":")[1]);
+            props.setProperty("driverClass", "com.mysql.jdbc.Driver");
+            props.setProperty("jdbcUrl", "jdbc:mysql://localhost:3306/calcul_mental");
+            props.setProperty("username", "root");
+            props.setProperty("password", "0000");
 
-
-            else
-             */
-
-            Properties props = new Properties();
-            props.setProperty("driverClass", "org.postgresql.Driver");
-            props.setProperty("jdbcUrl", "jdbc:postgres://ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/dnhnkr1kfvvb0");
-            props.setProperty("username", "abeqfgbsldfhfv");
-            props.setProperty("password", "cd2472481a06ab2311155ea82a7e325641f6cee2162bf61465fc025655439c43");
-            props.setProperty("ssl", "true");
             return props;
         }
     }
